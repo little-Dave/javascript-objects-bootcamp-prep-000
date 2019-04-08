9f7a401ac63d020638e106513ca3a0a31446439b
Dave Boehmer <daboehmer@gmail.com>
2019-04-08 02:56:22 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,3 +3,8 @@ var playlist = { "Jason Mraz": "Running"};
 function updatePlaylist(list, artist, song) {
   list[artist] = song;
 }
+
+function removeFromPlaylist(list, artist) {
+  delete list.artist;
+  return list;
+}
